e197024c68c8f4a420bd51a51180c3b54e37d47c
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2021-11-13 20:09:16 +0300

    added dropdown component functional

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import './index.scss'
 import { TextField } from './components/TextField/TextField'
+import { DropDown } from './components/DropDown/DropDown'
+
 
 TextField('[data-active="active"]')
+
+const drop = new DropDown('.dropdown', {
+  placeholder:'Удобства',
+  label:'Dropdown',
+  items:['спальни', 'кровати', 'ванные комнаты'],
+})
