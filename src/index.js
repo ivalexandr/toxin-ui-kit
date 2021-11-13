6de8cfd930d4ffc3324b66e5b79a3aac27fcd810
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2021-11-13 21:09:14 +0300

    ADD:added masked text field component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './index.scss'
 import { TextField } from './components/TextField/TextField'
 import { DropDown } from './components/DropDown/DropDown'
-
+import { MaskedTextFieldInit } from './components/MaskedTextField/MaskedTextField'
 
 TextField('[data-active="active"]')
 
-const drop = new DropDown('.dropdown', {
+new DropDown('.dropdown', {
   placeholder:'Удобства',
   label:'Dropdown',
   items:['спальни', 'кровати', 'ванные комнаты'],
 })
+
+MaskedTextFieldInit()
